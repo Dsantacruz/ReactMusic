e18c5a78fb83162068336efc250a3fdb155cecf8
edinson <jean_clude_dortosms@hotmail.com>
2017-03-26 00:30:36 -0300

    INIT

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//React
+
+const Search = (props) => {
+	return ('<div><h1>hola</h1></div>');
+}
+
+ReactDOM.render(
+
+	<Search name='Edinson'/>,
+	document.getElementById('home')
+
+)
